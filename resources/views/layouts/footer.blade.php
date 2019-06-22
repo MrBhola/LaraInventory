577f3061ab4ab3f7ab0ea8c1ca86b338559f5430
Bhola kr. Khawas <bkhawas42@gmail.com>
2019-06-22 15:32:07 +0545

    fooetr refactored

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,14 +1,15 @@
 <!-- Main Footer -->
+
 <footer class="main-footer">
-    <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">
-        Designed and developed by:
-        Bhola kr. Khawas
-        (bkhawas42@gmail.com)
-    </div>
-    <!-- Default to the left -->
+
     <strong>&copy; 2019-6-20 to {{now()}} <a href="https://github.com/allenkhawas" target="_blank">Bhola kr.
             Khawas</a>.</strong> All
     rights
     reserved.
+    <strong class="float-right">
+
+        Designed and developed by:
+        Bhola kr. Khawas
+        (bkhawas42@gmail.com)
+    </strong>
 </footer>
